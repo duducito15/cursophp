74523a79bdc83e0abe3ba0d3502fd6038c3a71be
Eduardo Chavez <echavezv@utea.edu.pe>
2023-04-05 14:59:32 +0200

    v1.5

--- ejemplo9.php
+++ ejemplo9.php
@@ -19,16 +19,28 @@
         $producto = $_POST["selProducto"];
 
         switch ($producto) {
-            case 'Lavadora': $precio = 1500; break;
-            case 'Refrigeradora': $precio = 3500; break;
-            case 'Radiograbadora': $precio = 2800; break;
-            case 'Tostadora': $precio = 500; break;
+            case 'Lavadora':
+                $precio = 1500;
+                break;
+            case 'Refrigeradora':
+                $precio = 3500;
+                break;
+            case 'Radiograbadora':
+                $precio = 2800;
+                break;
+            case 'Tostadora':
+                $precio = 500;
+                break;
         }
 
-        if ($producto=='Lavadora') $sell ='SELECTED'; else $sell='';
-        if ($producto=='Refrigeradora') $selre ='SELECTED'; else $selre='';
-        if ($producto=='Radiograbadora') $selra ='SELECTED'; else $selra='';
-        if ($producto=='Tostadora') $selt ='SELECTED'; else $selt='';
+        if ($producto == 'Lavadora') $sell = 'SELECTED';
+        else $sell = '';
+        if ($producto == 'Refrigeradora') $selre = 'SELECTED';
+        else $selre = '';
+        if ($producto == 'Radiograbadora') $selra = 'SELECTED';
+        else $selra = '';
+        if ($producto == 'Tostadora') $selt = 'SELECTED';
+        else $selt = '';
 
         ?>
         <form action="ejemplo9.php" method="post">
@@ -37,7 +49,7 @@
                     <td>Producto</td>
                     <td>
                         <select name="selProducto" onchange="this.form.submit()">
-                            <option value="Lavadora" <?php echo $sell; ?> >Lavadora</option>
+                            <option value="Lavadora" <?php echo $sell; ?>>Lavadora</option>
                             <option value="Refrigeradora" <?php echo $selre; ?>>Refrigeradora</option>
                             <option value="Radiograbadora" <?php echo $selra; ?>>Radiograbadora</option>
                             <option value="Tostadora" <?php echo $selt; ?>>Tostadora</option>
@@ -47,28 +59,33 @@
                 <tr>
                     <td>Precio</td>
                     <td>
-                        <input type="text" name="txtPrecio" readonly="readonly"
-                        value="<?php
-                            if ($_POST["selProducto"]) {
-                                echo number_format($precio,2,'.','');
-                            }
-                        ?>"
-                        >
+                        <input type="text" name="txtPrecio" readonly="readonly" value="<?php
+                                                                                        if ($_POST["selProducto"]) {
+                                                                                            echo number_format($precio, 2, '.', '');
+                                                                                        }
+                                                                                        ?>">
                     </td>
                 </tr>
+                <?php
+                $cantidad = $_POST["txtCantidad"];
+                $subtotal = $precio * $cantidad;
+                ?>
                 <tr>
                     <td>Cantidad</td>
                     <td>
-                        <input type="text" name="txtCantidad">
+                        <input type="text" name="txtCantidad" value="<?php echo $cantidad;?>">
                     </td>
                     <td>
                         <input type="submit" value="Calcular" name="btnCalcular">
                     </td>
-                </tr>
+                </tr>            
                 <tr>
                     <td>Subtotal</td>
                     <td>
-                        <input type="text" name="txtSubtotal">
+                        <input type="text" name="txtSubtotal" value="
+                        <?php
+                            echo number_format($subtotal, 2, '.', '');
+                        ?>" readonly="readonly">
                     </td>
                 </tr>
                 <tr>
