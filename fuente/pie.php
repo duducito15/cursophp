9798a9bc88b06530caf5b6228f2392a1b7d886c8
Eduardo Chavez <echavezv@utea.edu.pe>
2023-04-14 06:49:15 -0500

    Fuente 1.3

--- fuente/pie.php
+++ fuente/pie.php
@@ -1,28 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
     <link rel="stylesheet" href="../estilo.css">
-    <title>Fuente de soda</title>
 </head>
-
 <body>
-    <header>
-        <?php
-        include('encabezado.php');
-        ?>
-    </header>
-    <section>
-
-    </section>
-    <footer>
-        <?php
-        include('pie.php');
-        ?>
-    </footer>
+    <h6 id="centrado">Todos los derechos reservados - @2023 - Eduardo</h6>
 </body>
-
 </html>
