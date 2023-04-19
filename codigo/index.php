59e0602b47fdbaf490c1c509b1b3ac549dd9d1a9
Eduardo Chavez <echavezv@utea.edu.pe>
2023-04-19 07:07:00 -0500

    CODIGO V1.1

---
+++ codigo/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registro de empleados</title>
+    <link rel="stylesheet" href="../estilo.css">
+</head>
+
+<body>
+    <header>
+        <h2 id="centrado">Formulario de registro de empleados</h2>
+        <img src="https://img.freepik.com/fotos-premium/equipo-gente-negocios-trabajando-juntos-oficina-hablando-discutiendo-banner-fondo-trabajo-equipo_44344-3554.jpg" alt="" height="150" width="700">
+    </header>
+    <section>
+        <form action="index.php" method="post">
+            <table border="0" width="700" cellspacing="0" cellpading ="0">
+                <tr>
+                    <td>Apellidos: </td>
+                    <td><input type="text" name="txtApellidos" size="70" placeholder="Ingrese apellidos"></td>
+                    <td>CODIGO GENERADO</td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                </tr>   
+
+            </table>
+
+        </form>
+    </section>
+    <footer>
+
+    </footer>
+
+
+</body>
+
+</html>
