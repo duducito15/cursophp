a9260d7664bde0aa3c70dd5f6d547a156425d2d5
Eduardo Chavez <echavezv@utea.edu.pe>
2023-05-05 07:34:35 -0500

    BD v3 - mysqli_query

--- bd/conexion.php
+++ bd/conexion.php
@@ -1,29 +1,55 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Conexion</title>
 </head>
+
 <body>
-    
+
     <?php
     //Conectarse a la base de datos, usamos la siguiente función 
     //mysqli_connect('SERVIDOR', 'USUARIO', 'CLAVE');
 
     $servidor = 'localhost';
     $usuario = 'root';
-    $clave ='123456';
+    $clave = '123456';
 
     $cn = mysqli_connect($servidor, $usuario, $clave);
-    if(!$cn){
+    if (!$cn) {
         echo "No se pudo conectar al servidor";
-    } else{
+    } else {
         echo "Conección satisfactoria";
     }
-
-    ?>
+    // $cn = mysqli_connect($servidor, $usuario, $clave) or
+    //         die ('Error en la conección->'.mysqli_error($cn));
     
+    //Seleccionar la Base de datos
+    $db = mysqli_select_db($cn, 'ventas2015');
+    if (!$db) {
+        echo "<br>La BD no existe";
+    } else {
+        echo "<br>BD seleccionada satisfactoriamente!!!";
+    }
+
+    //consultas MySql
+    $rs = mysqli_query($cn, "SELECT * FROM CLIENTE");
+    if ($rs) {
+        echo "<br>Consulta implementada correctamente";
+    }else {
+        echo "<br>Error al implementar la consulta!!!";
+    }
+    
+    //Cerrar la conexiòn al Gestor de Base de datos
+    $ccn = mysqli_close($cn);
+    if ($ccn) {
+        echo "<br>Conección cerrada correctamente...!!!";
+    }
+    ?>
+
 </body>
+
 </html>
