c09c1f87d1d76f7c7ed641e6c87327ad786cdca6
Eduardo Chavez <echavezv@utea.edu.pe>
2023-05-10 07:27:48 -0500

    LISTADO V2

--- bd/listadoClientes.php
+++ bd/listadoClientes.php
@@ -11,10 +11,35 @@
 <body>
     <?php
         include('cnventas.php');
-        $rs = mysqli_query($cn, "SELECT * FROM CLIENTE");
+        $rs = mysqli_query($cn, "SELECT C.ID_CLIENTE AS CODIGO, 
+                        CONCAT(C.NOMBRES,' ',C.PATERNO,' ',C.MATERNO) AS CLIENTE, 
+                        D.DESCRIPCION AS DISTRITO FROM CLIENTE C INNER JOIN DISTRITO D 
+                        ON C.ID_DISTRITO = D.ID_DISTRITO");
         $r = mysqli_fetch_assoc($rs);
     ?>
+<header>
+    <h3>LISTADO DE CLIENTES</h3>
+</header>
+<section>
+<table border="0" width="550">
+    <tr>
+        <td>CODIGO</td>
+        <td>CLIENTE</td>
+        <td>DISTRITO</td>
+    </tr>
+    <?php foreach($rs as $r){?>
+    <tr>
+        <td><?php echo $r['CODIGO']?></td>
+        <td><?php echo $r['CLIENTE']?></td>
+        <td><?php echo $r['DISTRITO']?></td>
+    </tr>
+    <?php } ?>
 
+</table>
+</section>
+<footer>
+    <h6>Todos los derechos reservados @2023</h6>
+</footer>
 </body>
 
 </html>
