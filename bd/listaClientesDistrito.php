387942ab833bd65413792c093e309ef72447bc2c
Eduardo Chavez <echavezv@utea.edu.pe>
2023-05-12 07:56:44 -0500

    LC v2

--- bd/listaClientesDistrito.php
+++ bd/listaClientesDistrito.php
@@ -31,20 +31,55 @@
                              C.ID_DISTRITO = D.ID_DISTRITO;");
                             $n = mysqli_num_rows($rs);
 
-                            foreach($rs as $r){
+                            foreach ($rs as $r) {
                                 $codigo = $r['CODIGO'];
                                 $distrito = $r['DISTRITO'];
-                                echo "<option value= ' ".$codigo. " '> ".$distrito."</option>";
-                                
+                                echo "<option value= ' " . $codigo . " '> " . $distrito . "</option>";
                             }
                             ?>
                         </select>
                     </td>
+                    <td>
+                        <input type="submit" name="btnListar" value="LISTAR">
+                    </td>
                 </tr>
             </table>
-        </form>
 
-        </table>
+            <?php
+            include('cnventas.php');
+            $codigoDistrito = $_POST['selDistrito'];
+            echo $codigoDistrito;
+            $consulta = "SELECT C.ID_CLIENTE AS CODIGO, 
+                            CONCAT(C.NOMBRES,' ', C.PATERNO,' ',C.MATERNO) AS CLIENTE, 
+                            FONO AS TELEFONO, DESCRIPCION AS DISTRITO 
+                            FROM CLIENTE C INNER JOIN DISTRITO D 
+                            ON C.ID_DISTRITO = D.ID_DISTRITO
+                            WHERE C.ID_DISTRITO = '.$codigoDistrito.'";
+            $rs1 = mysqli_query($cn, $consulta);
+            echo json_encode($rs1);
+
+            $total = mysqli_num_rows($rs1);
+            echo $total;
+            ?>
+
+            <h4>CLIENTES POR DISTRITO: <?php echo $codigoDistrito ?></h5>
+                <table border="0" width="700" cellpadding="1" cellspacing="1">
+                    <tr>
+                        <th>CODIGO</th>
+                        <th>CLIENTE</th>
+                        <th>TELEFONO</th>
+                        <th>DISTRITO</th>
+                    </tr>
+                    <?php foreach ($rs1 as $r1) { ?>
+                        <tr>
+                            <td><?php echo $r1['CODIGO'] ?></td>
+                            <td><?php echo $r1['CLIENTE'] ?></td>
+                            <td><?php echo $r1['TELEFONO'] ?></td>
+                            <td><?php echo $r1['DISTRITO'] ?></td>
+                        </tr>
+                    <?php } ?>
+                </table>
+        </form>
     </section>
     <footer>
         <h6>Todos los derechos reservados @2023</h6>
