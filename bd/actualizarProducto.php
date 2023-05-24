1167b77ea54e64c3b5194b1d9641ecfac71833d5
Eduardo Chavez <echavezv@utea.edu.pe>
2023-05-24 06:55:21 -0500

    AP V2

--- bd/actualizarProducto.php
+++ bd/actualizarProducto.php
@@ -11,10 +11,50 @@
 
 <body>
     <header>
-        <h3>ACTUALIZACIÒN DE DATOS DEL PRODUCTO</h3>
+        <h3>ACTUALIZACIÓN DE DATOS DEL PRODUCTO</h3>
     </header>
     <section>
-
+        <form action="actuzalizarProducto.php" method="post">
+            <table width="550" border="1">
+                <tr>
+                    <td>Ingrese codigo del producto</td>
+                    <td>
+                        <input type="text" name="txtCodigo">
+                    </td>
+                    <td>
+                        <input type="submit" name="btnBuscar" value="BUSCAR">
+                    </td>
+                </tr>
+            </table>
+            <table width="550" border="1">
+                <tr>
+                    <td>DESCRIPCIÓN</td>
+                    <td colspan="3">
+                        <input type="text" name="txtDescripcion" value="">
+                    </td>
+                </tr>
+                <tr>
+                    <td>PRECIO</td>
+                    <td>
+                        <input type="text" name="txtPrecio">
+                    </td>
+                    <td>STOCK</td>
+                    <td>
+                        <input type="text" name="txtStock">
+                    </td>
+                </tr>
+                <tr>
+                    <td>FECHA VENCIMIENTO</td>
+                    <td>
+                        <input type="text" name="txtFecha">
+                    </td>
+                    <td colspan="2">
+                        <input type="submit" value="ACTUALIZAR">
+                    </td>
+                    
+                </tr>
+            </table>
+        </form>
     </section>
     <footer>
         <h5>Todos los derechos rservados @2023</h5>
