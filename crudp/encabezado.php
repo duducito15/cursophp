93f1e8bde670f3903b6f01feee770ee67ff7e6ee
Eduardo Chavez <echavezv@utea.edu.pe>
2023-05-31 07:23:17 -0500

    CRUD PRO v1

---
+++ crudp/encabezado.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>MANTENIMIENTO PRODUCTO</title>
+</head>
+<body>
+    <h3>ADMINISTRAR PRODUCTOS</h3>
+</body>
+</html>
---
+++ crudp/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+<body>
+    <header>
+        
+    </header>
+    <section>
+        <table >
+
+        </table>
+    </section>
+    
+</body>
+</html>
