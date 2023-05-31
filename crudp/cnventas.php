d69bb16bacda59d00305003cf7f6db22f2c78f33
Eduardo Chavez <echavezv@utea.edu.pe>
2023-05-31 07:47:59 -0500

    CRUD PRODUCTO v2

---
+++ crudp/cnventas.php
@@ -0,0 +1,6 @@
+<?php
+$servidor ="localhost";
+$usuario = "root";
+$clave = "123456";
+$cn = mysqli_connect($servidor, $usuario, $clave, "ventas2015");
+//mysqli_select_db($cn,"ventas2015");
--- crudp/index.php
+++ crudp/index.php
@@ -1,18 +1,67 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title></title>
 </head>
+
 <body>
     <header>
-        
+        <?php
+        include('encabezado.php');
+        include('cnventas.php')
+
+        ?>
     </header>
     <section>
-        <table >
+        <table width="550">
+            <tr>
+                <td>Adminsitrar Productos</td>
+                <td align="right">
+                    <input type="button" value="Agregar Nuevo Producto">
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2" align="right">
+                    <input type="text">
+                    <input type="button" value="Buscar">
+                </td>
+            </tr>
+        </table>
+        <table border="0" cellpading="5" cellspacing="0" width="550">
+            <tr>
+                <th>CODIGO</th>
+                <th>DESCRIPCIÓN</th>
+                <th>PRECIO</th>
+                <th>STOCK</th>
+                <th>FECHA VENCIMIENTO</th>
+                <th></th>
+                <th></th>
+            </tr>
+            <?php
+            $rs = mysqli_query($cn, "CALL sp_productoListar");
+            foreach ($rs as $r) { ?>
+                <tr>
+                    <td><?php echo $r['COD'] ?></td>
+                    <td><?php echo $r['DES'] ?></td>
+                    <td><?php echo $r['PRE'] ?></td>
+                    <td><?php echo $r['STO'] ?></td>
+                    <td><?php echo $r['FEC'] ?></td>
+                    <td><?php echo "Actualizar" ?></td>
+                    <td><?php echo "Eliminar" ?></td>
+                </tr>
+            <?php } ?>
 
         </table>
+
     </section>
-    
+    <footer>
+        <?php
+        include('pie.php');
+        ?>
+    </footer>
+
 </body>
+
 </html>
